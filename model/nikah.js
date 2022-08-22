e3cc2d01bb72be535a3e4d8fd7654e03029da6b4
abunaum <abunaum@hotmail.com>
2022-08-22 17:15:07 +0700

    add data nikah

---
+++ model/nikah.js
@@ -0,0 +1,137 @@
+const mongoose = require("mongoose");
+
+const Nikah = mongoose.model('Nikah',{
+    noreg: {
+        type: String,
+        required: true,
+    },
+    tglregister: {
+        type: Date,
+        required: true,
+    },
+    nikpr: {
+        type: Number,
+        required: true,
+    },
+    statuspr: {
+        type: String,
+        required: true,
+    },
+    noacpr: {
+        type: String,
+        required: false,
+    },
+    sttaypr: {
+        type: String,
+        required: true,
+    },
+    nikaypr: {
+        type: Number,
+        required: false,
+    },
+    binaypr: {
+        type: String,
+        required: false,
+    },
+    namaaypr: {
+        type: String,
+        required: false,
+    },
+    sttibpr: {
+        type: String,
+        required: true,
+    },
+    nikibpr: {
+        type: Number,
+        required: false,
+    },
+    bintiibpr: {
+        type: String,
+        required: false,
+    },
+    namaibpr: {
+        type: String,
+        required: false,
+    },
+    niklk: {
+        type: Number,
+        required: true,
+    },
+    binlk: {
+        type: String,
+        required: true,
+    },
+    statuslk: {
+        type: String,
+        required: true,
+    },
+    noaclk: {
+        type: String,
+        required: false,
+    },
+    sttkeldes: {
+        type: String,
+        required: true,
+    },
+    sttaylk: {
+        type: String,
+        required: false,
+    },
+    nikaylk: {
+        type: Number,
+        required: false,
+    },
+    binaylk: {
+        type: String,
+        required: false,
+    },
+    namaaylk: {
+        type: String,
+        required: false,
+    },
+    sttiblk: {
+        type: String,
+        required: false,
+    },
+    nikiblk: {
+        type: Number,
+        required: false,
+    },
+    bintiiblk: {
+        type: String,
+        required: false,
+    },
+    namaiblk: {
+        type: String,
+        required: false,
+    },
+    walinikah: {
+        type: String,
+        required: true,
+    },
+    nikwali: {
+        type: String,
+        required: false,
+    },
+    hubungan_wali: {
+        type: String,
+        required: false,
+    },
+    tempat_nikah: {
+        type: String,
+        required: true,
+    },
+    tglnikah: {
+        type: Date,
+        required: true,
+    },
+    mas_kawin: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    }
+});
+module.exports = Nikah;
--- routers/nikah.js
+++ routers/nikah.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const Nikah = require("../model/nikah");
+const bodyParser = require("body-parser");
+const Orang = require("../model/orang");
+const {toDate} = require("validator");
 const nikah = express.Router();
 
 function IsLoggedIn(req, res, next) {
@@ -30,10 +34,12 @@ function IsLoggedIn(req, res, next) {
     }
 }
 
-nikah.get('/masuk',IsLoggedIn, (req, res) => {
+nikah.get('/masuk',IsLoggedIn, async (req, res) => {
+    const datanikah = await Nikah.find();
     res.render('nikah/list_masuk', {
         title: 'Nikah Masuk',
         info : req.session,
+        datanikah,
         sukses : req.flash('sukses'),
         error : req.flash('error'),
         gagal : req.flash('gagal'),
@@ -48,6 +54,22 @@ nikah.get('/masuk/tambah',IsLoggedIn, (req, res) => {
         error : req.flash('error'),
         gagal : req.flash('gagal'),
     });
+});
+nikah.post('/masuk/tambah',IsLoggedIn, (req, res) => {
+    const status = {status: 'masuk'};
+    const datareq = req.body;
+    const tglnikah = toDate(datareq.tglnikah);
+    datareq.tglnikah = tglnikah;
+    const data = Object.assign(datareq, status)
+    Nikah.create(data, function (err) {
+        if (err) {
+            req.flash('gagal',err);
+        }
+        else {
+            req.flash('sukses','Berhasil menambah data Nikah');
+        }
+    });
+    res.redirect('/nikah/masuk');
 });
 
 module.exports = nikah;
