008f68cf07006e24b82b92624abc3e07a2dda4b9
abunaum <abunaum@hotmail.com>
2022-08-28 13:18:27 +0700

    print view N1 Wanita

--- controller/cetak.js
+++ controller/cetak.js
@@ -1,9 +1,13 @@
 const Nikah = require("../model/nikah");
 const {filter_masuk} = require("../utils/filter_nikah");
+const moment = require("moment/moment");
 module.exports = {
     index: async function (req,res){
         const detailreg = await Nikah.findById(req.params.id);
         const dr = await filter_masuk(detailreg);
-        res.send('model_nikah/n1wanita');
+        res.render('model_nikah/n1wanita', {
+            title: 'N1',
+            dr,
+        });
     }
 }
