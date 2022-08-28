c09e84d2e7559d3d148ba08bf2ab6467afdc426f
abunaum <abunaum@hotmail.com>
2022-08-28 14:19:00 +0700

    print view N1 pria & N2

--- controller/cetak.js
+++ controller/cetak.js
@@ -2,11 +2,27 @@ const Nikah = require("../model/nikah");
 const {filter_masuk} = require("../utils/filter_nikah");
 const moment = require("moment/moment");
 module.exports = {
-    index: async function (req,res){
+    n1pr: async function (req,res){
         const detailreg = await Nikah.findById(req.params.id);
         const dr = await filter_masuk(detailreg);
         res.render('model_nikah/n1wanita', {
             title: 'N1',
+            dr,
+        });
+    },
+    n1lk: async function (req,res){
+        const detailreg = await Nikah.findById(req.params.id);
+        const dr = await filter_masuk(detailreg);
+        res.render('model_nikah/n1pria', {
+            title: 'N1',
+            dr,
+        });
+    },
+    n2: async function (req,res){
+        const detailreg = await Nikah.findById(req.params.id);
+        const dr = await filter_masuk(detailreg);
+        res.render('model_nikah/n2', {
+            title: 'N2',
             dr,
         });
     }
--- routers/cetak.js
+++ routers/cetak.js
@@ -3,6 +3,8 @@ const cetakmodel = express.Router();
 const controller = require('../controller/cetak');
 
 
-cetakmodel.get('/n1wanita/:id', controller.index);
+cetakmodel.get('/n1wanita/:id', controller.n1pr);
+cetakmodel.get('/n1pria/:id', controller.n1lk);
+cetakmodel.get('/n2/:id', controller.n2);
 
 module.exports = cetakmodel;
